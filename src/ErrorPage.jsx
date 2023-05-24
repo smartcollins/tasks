13d6a91c1322648e813c95c8cc7097908f2a87fa
collins <kevin1520smart@gmail.com>
2023-05-24 22:23:26 +0100

    created ErrorPage

---
+++ src/ErrorPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const ErrorPage = ()=>{
+    return(
+        <div className='texxt-xl'>
+            <p className=' text-4xl'>404</p>
+            <p>file not found</p>
+        </div>
+    )
+}
+
+
+export default ErrorPage; 
